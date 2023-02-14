9aff95783dc5a0c9f26933accd46d6ca13023c41
Сергей Филатов <i@sergei-filatov.ru>
2023-02-15 01:30:37 +0300

    ДЗ 6 спринт

--- yatube/core/tests/test_custom_errors.py
+++ yatube/core/tests/test_custom_errors.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.http import HttpResponseForbidden, Http404
 
 
@@ -6,12 +6,14 @@ class TestCustomErrors(TestCase):
     """
     Тест шаблонов кастомных ошибок
     """
+    def setUp(self) -> None:
+        self.guest_client = Client
     def test_custom_templates(self):
         self.assertTemplateUsed(
-            Http404,
+            self.guest_client.get(Http404),
             'core/404.html'
         )
         self.assertTemplateUsed(
-            HttpResponseForbidden,
+            self.guest_client.get(HttpResponseForbidden),
             'core/403csrf.html'
         )
